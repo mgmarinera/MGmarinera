40e6270dd8bbce3ac422d725e92f3104d5674d62
mgmarinera <111923984+mgmarinera@users.noreply.github.com>
2022-08-24 13:54:14 +0200

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,10 +1,11 @@
 <?php
     $destino="mg.marinera.es@gmail.com";
-    $nombre=$_POST["Nombre"];
-    $correo=$_POST["Correo"];
-    $phone=$_POST["Telefono"];
-    $mensaje=$_POST["Mensaje"];
+    $nombre=$_POST["nombre"];
+    $correo=$_POST["correo"];
+    $phone=$_POST["telefono"];
+    $mensaje=$_POST["mensaje"];
     $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $phone . "\nMensaje: " . $mensaje;
     mail($destino,"Contacto",$contenido);
-    header("Location:index.html");
+    echo "<script>alert('Correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
