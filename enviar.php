dce1ea10ca5945088ede910d6f8bd782de26a35d
mgmarinera <111923984+mgmarinera@users.noreply.github.com>
2022-08-24 12:50:32 +0200

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -2,9 +2,9 @@
     $destino="mg.marinera.es@gmail.com";
     $nombre=$_POST["Nombre"];
     $correo=$_POST["Correo"];
-    $phone=$_POST["Teléfono"];
+    $phone=$_POST["Telefono"];
     $mensaje=$_POST["Mensaje"];
-    $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $phone . "\nMensaje: " . $mensaje
+    $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $phone . "\nMensaje: " . $mensaje;
     mail($destino,"Contacto",$contenido);
-    header("Location:index.html");
+    header("Location:https://mgmarinera.github.io/MGmarinera/index.html");
 ?>
